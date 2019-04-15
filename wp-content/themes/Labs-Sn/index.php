c5f254d8560a9832978e0d7b76adc934c220bb7b
SnYoussef <SnYoussef@AaA.be>
2019-04-15 11:45:26 +0200

    Ajout des Templates header et footer et du dossier templates

---
+++ wp-content/themes/Labs-Sn/index.php
@@ -0,0 +1,13 @@
+<?php
+
+get_header();
+
+get_template_part("templates/introSection");
+get_template_part("templates/aboutSection");
+get_template_part("templates/testimonialSection");
+get_template_part("templates/servicesSection");
+get_template_part("templates/teamSection");
+get_template_part("templates/promotionSection");
+get_template_part("templates/contactSection");
+
+get_footer();
