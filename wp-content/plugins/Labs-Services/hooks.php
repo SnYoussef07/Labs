024464a135d812e4aefd694037d8fa525d7034fc
SnYoussef <SnYoussef@AaA.be>
2019-04-16 10:07:59 +0200

    Création class Database et CreateServiceTable pour creé une table dans la base de donné

--- wp-content/plugins/Labs-Services/hooks.php
+++ wp-content/plugins/Labs-Services/hooks.php
@@ -1,5 +1,12 @@
 <?php
 
 use App\Features\PostTypes\ServicePostType;
+use App\Database\Database;
+
 
 add_action('init', [ServicePostType::class, 'register']);
+
+
+
+register_activation_hook(__DIR__ . '/LabServices.php', [Database::class, 'init']);
+
