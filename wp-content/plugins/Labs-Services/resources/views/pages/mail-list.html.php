a09970518b74365bdd0f93715c155958ed4238e1
SnYoussef <SnYoussef@AaA.be>
2019-04-19 13:29:26 +0200

    Ajout de la page Mail

---
+++ wp-content/plugins/Labs-Services/resources/views/pages/mail-list.html.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Mail</title>
+</head>
+<body>
+   <div>
+   <?php foreach ($mails as $mail): ?>
+            <div class="postbox mt-3">
+                <div class="inside">
+                    <strong>client : </strong><?=$mail->email;?>
+                </div>
+            </div>
+            <?php endforeach;?>
+   </div>
+</body>
+</html>
