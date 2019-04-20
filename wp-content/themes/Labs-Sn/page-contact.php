ae7f8601d3b3146de3bf4050bfb4b14deb32b222
SnYoussef <SnYoussef@AaA.be>
2019-04-20 11:13:26 +0200

    Ajout page contact

---
+++ wp-content/themes/Labs-Sn/page-contact.php
@@ -0,0 +1,15 @@
+<?php
+
+get_header();
+
+get_template_part("templates/headerServices");
+
+?>
+
+<img src="<?php echo get_template_directory_uri(); ?>/img/mapMolen.png" alt="">
+
+
+<?php
+get_template_part("templates/contactForm");
+
+get_footer();
