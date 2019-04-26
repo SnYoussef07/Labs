0e7e44cb17c0f4a4415c51358eb096aa8ef19d64
SnYoussef <SnYoussef@AaA.be>
2019-04-26 20:55:58 +0200

    Ajout archive pour tag et category

--- wp-content/themes/Labs-Sn/archive.php
+++ wp-content/themes/Labs-Sn/archive.php
@@ -1,19 +1,18 @@
 <?php
-$paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
-$args = [
-    'post_type' => 'post',
-	'posts_per_page' => 3,
-	'paged'          => $paged
-];
-$query = new WP_Query($args);
+get_header();
+get_template_part("templates/headerServices");
 ?>
-<!-- page section -->
+
+
 <div class="page-section spad">
 		<div class="container">
 			<div class="row">
 
 				<div class="col-md-8 col-sm-7 blog-posts">
-                <?php while ($query->have_posts()): $query->the_post();?>
+                <h1><?php echo 'Recherche pour: ' . get_query_var('tag') . get_query_var('category_name') ?></h1>
+                <br>
+                <br>
+                <?php while (have_posts()): the_post();?>
 								<!-- Post item -->
 								<div class="post-item">
 									<div class="post-thumbnail">
@@ -27,35 +26,24 @@ $query = new WP_Query($args);
 										<h2 class="post-title"><?=the_title();?></h2>
 										<div class="post-meta">
 		                                    <a href=""><?=the_author()?></a>
-									        
+									        <a href="">
 											<!-- ici la boucle-->
 											<?php
 												$allTags = get_the_tags();
-												foreach($allTags as $tag){?>
-													<a href="<?php echo get_tag_link($tag); ?>"><?=  $tag->name; ?></a>
-													
-												<?php }?>	
+												foreach($allTags as $tag){
+													echo $tag->name . ' ,';
+												}
+                                    		?>	
+		                                    </a>
 		                                    <a href=""><?=get_comments_number()?> Comments</a>
 										</div>
 										<p><?=the_excerpt();?></p>
 										<a href="<?=the_permalink(get_the_ID())?>" class="read-more">Read More</a>
 									</div>
 			                    </div>
-					        <?php endwhile;?>
-                    <!-- Pagination -->
-					<div class="myRow"> 
-						<div><?php previous_posts_link('<button type="button" class="btn bgColor btn-arrow-left">Prev</button>');?></div>
-						<div><?php next_posts_link( ' <button type="button" class="btn bgColor btn-arrow-right">
-						Next
-						</button>', $query->max_num_pages );?>
-						</div>
-					</div>
-
+					        <?php endwhile;?>					
                 </div>
-
-                <!-- SUITS -->
-
-				<!-- Sidebar area -->
+                <!-- Sidebar area -->
 				<div class="col-md-4 col-sm-5 sidebar">
 					<?php get_template_part("templates/searchForm"); ?>
 					<?php dynamic_sidebar('sidebar-1');?>
@@ -70,8 +58,10 @@ $query = new WP_Query($args);
 						</ul>
 					</div>
 				</div>
-
 			</div>
 		</div>
-	</div>
-	<!-- page section end-->
+    </div>
+    
+    <?php
+    get_footer();
+    ?>
