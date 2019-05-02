45ac6404f0131fe2453965098628fcb4ec6df335
SnYoussef <SnYoussef@AaA.be>
2019-05-02 10:55:25 +0200

    Mj

--- wp-content/themes/Labs-Sn/functions.php
+++ wp-content/themes/Labs-Sn/functions.php
@@ -8,3 +8,12 @@ require_once get_template_directory() . '/includes/customizer.php';
 
 require_once get_template_directory() . '/includes/sidebars.php';
 
+
+function wpb_search_filter($query)
+{
+    if ($query->is_search) {
+        $query->set('post_type', 'post');
+    }
+    return $query;
+}
+add_filter('pre_get_posts', 'wpb_search_filter');
